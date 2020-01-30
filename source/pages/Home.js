fa7f69739d1aaaaccbdade8262e70364cee45ec3
Alistair Fink <alistairfink@gmail.com>
2020-01-29 20:05:24 -0500

    interim

--- source/pages/Home.js
+++ source/pages/Home.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import HomeStyles from '../styles/HomeStyles';
 
 function Home() {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text>Test</Text>
-      </ScrollView>
+      <View style={HomeStyles.Outer}>
+        <View style={HomeStyles.TitleBar}>
+          <Text style={HomeStyles.Title}>CycleVision</Text>
+          <TouchableOpacity style={HomeStyles.SettingsButtonOuter}>
+            <Image
+              style={HomeStyles.SettingsButton}
+              source={require('../resources/SettingsMenuButton.png')}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
--- source/styles/HomeStyles.js
+++ source/styles/HomeStyles.js
@@ -1,6 +1,21 @@
 import {StyleSheet} from 'react-native';
 
 const HomeStyles = StyleSheet.create({
+  Outer: {
+    height: '100%',
+    backgroundColor: 'red',
+  },
+  TitleBar: {
+    backgroundColor: 'blue',
+    height: '20%',
+    flexDirection: 'row',
+  },
+  Title: {
+    color: 'white',
+  },
+  SettingsButtonOuter: {
+    marginTop: '1',
+  },
 });
 
 export default HomeStyles;
