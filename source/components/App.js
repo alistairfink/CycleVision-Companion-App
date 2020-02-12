3f1e2c0391a25b4616312b584566716c6fad4c91
Alistair Fink <alistairfink@gmail.com>
2020-02-11 22:27:12 -0500

    change starting back to home

--- source/components/App.js
+++ source/components/App.js
@@ -19,7 +19,7 @@ const Navigator = createStackNavigator(
 	},
 	{
 		headerMode: 'none',
-		initialRouteName: 'Test',
+		initialRouteName: 'Home',
 		defaultNavigationOptions: {
 			...TransitionPresets.SlideFromRightIOS,
 		},
