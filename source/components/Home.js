3eb5837db1259bbd8822d953ad30d31701b243fa
Alistair Fink <alistairfink@gmail.com>
2020-03-06 08:09:11 -0500

    only work if connected to device

--- source/components/Home.js
+++ source/components/Home.js
@@ -8,6 +8,7 @@ import {
   StatusBar,
   TouchableOpacity,
   Image,
+  Alert,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
@@ -21,23 +22,52 @@ import BatteryIndicator from './BatteryIndicator';
 import SettingsMenu from './SettingsMenu';
 
 // Utilities
-import {DEVICE_URL_KEY, DEFAULT_DEVICE_IP} from '../utilities/Constants';
+import {
+  DEVICE_URL_KEY,
+  DEFAULT_DEVICE_IP,
+  HEALTH_CHECK_API,
+} from '../utilities/Constants';
+import FetchWithTimeout from '../utilities/FetchWithTimeout';
 
 function Home({navigation}) {
+  const [healthCheckURL, setHealthCheckURL] = useState(null);
+
   useEffect(() => {
     let setLocalSettings = async () => {
       try {
         const value = await AsyncStorage.getItem(DEVICE_URL_KEY);
         if (value === null) {
+          value = DEFAULT_DEVICE_IP;
           await AsyncStorage.setItem(DEVICE_URL_KEY, DEFAULT_DEVICE_IP);
         }
+
+        setHealthCheckURL(value + HEALTH_CHECK_API);
       } catch (e) {
         await AsyncStorage.setItem(DEVICE_URL_KEY, DEFAULT_DEVICE_IP);
       }
     };
 
     setLocalSettings();
-  }, []);
+  }, [setHealthCheckURL]);
+
+  const start = () => {
+    if (healthCheckURL !== null) {
+      FetchWithTimeout(healthCheckURL, {}, 5000)
+        .then(result => {
+          navigation.navigate('StartRide');
+        })
+        .catch(e => {
+          cantConnect();
+        });
+    } else {
+      cantConnect();
+    }
+  };
+
+  const cantConnect = () => {
+    Alert.alert('Connection Error', 'Unable to connect to device.');
+  };
+
   return (
     <SafeAreaView>
       <StatusBar barStyle="light-content" backgroundColor={Colours.Primary} />
@@ -50,7 +80,7 @@ function Home({navigation}) {
         </View>
         <View style={HomeStyles.HomeMainOuter}>
           <TouchableOpacity
-            onPress={() => navigation.navigate('StartRide')}
+            onPress={() => start()}
             style={HomeStyles.HomeStartOuter}>
             <Text style={HomeStyles.StartRideTitle}>Start Ride</Text>
             <Image
--- source/utilities/Constants.js
+++ source/utilities/Constants.js
@@ -3,3 +3,4 @@ export const DEFAULT_DEVICE_IP = 'http://10.49.165.133:41691';
 
 export const VIDEO_API = '/api/video';
 export const EVENT_API = '/api/event';
+export const HEALTH_CHECK_API = '/api/alive';
